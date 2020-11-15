5f83fd828d76ba255e1e9dd2a517e87883e9c989
Nathalie Freyburger <nfreyburger.pers0@gmail.com>
2020-11-15 13:47:54 +0100

    rails update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,10 +22,11 @@ class CocktailsController < ApplicationController
     end
   end
 
- def destroy
-  @cocktail.destroy
-  redirect_to cocktails_path
-end
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
 
   private
 
@@ -33,4 +34,3 @@ end
     params.require(:cocktail).permit(:name, :photo)
   end
 end
-

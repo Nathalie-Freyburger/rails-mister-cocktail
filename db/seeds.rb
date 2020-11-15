452ce9291a83b2b7a1d683ea2bbdce68bd0f4b3b
Nathalie Freyburger <nfreyburger.pers0@gmail.com>
2020-11-15 11:56:34 +0100

    rails update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,3 @@ ingredients = JSON.parse(ingredients_serialized)
 ingredients["drinks"].each do |ingredient|
     Ingredient.create(name: ingredient["strIngredient1"])  
 end
-
-url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
-cocktails_serialized = open(url).read
-cocktails = JSON.parse(cocktails_serialized)
-
-cocktails["drinks"].each do |cocktail|
-  Cocktail.create(name: cocktail["strDrink"])
-end
